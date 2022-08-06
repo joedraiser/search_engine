78a51768f1011220f32f63f4779c6eb6a0ca6f04
Evgenii.Korchagov <joedraiser@gmail.com>
2022-08-06 13:04:07 +0200

    Added recourse files validation(1000 words, 100 word length)

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <exception>
 #include <iomanip>
-#include <sstream>
 
 std::string CONFIG_FILE=".//config.json";
 
@@ -38,21 +37,58 @@ class ConverterJSON
         try {
             std::vector<std::string> textDocuments;
             std::ifstream temp_strm(CONFIG_FILE);
+
+            /** check if config file opened */
             if (!temp_strm.is_open())
                 throw missing_config();
+            /** read config file */
             nlohmann::json config_json;
             temp_strm >> config_json;
             temp_strm.close();
-            std::stringstream buff;
 
+            std::stringstream buff;
+            std::string word_to_check;
+            int word_count=1;
+
+            /** iterate through resources files */
             for (auto fileNames_it = config_json["files"].begin();
             fileNames_it != config_json["files"].end();
-            fileNames_it++)
+            fileNames_it++
+            )
             {
+                /** open resource file */
                 temp_strm.open(fileNames_it.value());
+                if(!temp_strm.is_open())
+                {
+                    std::cout << fileNames_it.value() << " is missing\n";
+                    continue;
+                }
+
                 buff << temp_strm.rdbuf();
+
+
+                while(!buff.eof())
+                {
+                    buff >> word_to_check;
+                    if(word_to_check.length()>100)
+                    {
+                        std::cout << fileNames_it.value() << " contains very lengthy word";
+                        std::stringstream().swap(buff);
+                        break;
+                    }
+                    else if(word_count > 1000)
+                    {
+                        std::cout << fileNames_it.value() << " contains too much words";
+                        std::stringstream().swap(buff);
+                        break;
+                    }
+                    word_count++;
+                }
+                word_count=1;
+
                 textDocuments.push_back(buff.str());
                 temp_strm.close();
+                /** crear the buffer */
                 std::stringstream().swap(buff);
             }
 
@@ -118,6 +154,7 @@ class ConverterJSON
         std::string request_count_string;
         std::stringstream request_ss;
 
+        /** iterate throgh requests*/
         for(auto request = answers.begin(); request != answers.end(); request++)
         {
             /**Check if there any relevances at all*/
@@ -134,16 +171,20 @@ class ConverterJSON
             {
                 request_json["result"] = "false";
             }
+            /** so there are relevances. Add them to request */
             else
             {
+                /** iterate through docids*/
                 for(auto docid=request->begin(); docid!=request->end(); docid++)
                 {
+                    /** if there is only one relevance */
                     if(docid->second > 0 && docid_count == 0)
                     {
                         request_json["docid"] = docid->first;
                         request_json["rank"] = docid->second;
                         docid_count++;
                     }
+                    /** if there is two relevances */
                     else if(docid->second > 0 && docid_count == 1)
                     {
                         /** add docid and rank that already in config into array*/
@@ -160,6 +201,7 @@ class ConverterJSON
 
                         docid_count++;
                     }
+                    /** if there are many relevances */
                     else if(docid->second > 0 && docid_count > 1)
                     {
                         relevance_json.push_back(
@@ -173,6 +215,7 @@ class ConverterJSON
                 docid_count = 0;
                 relevance_json.clear();
             }
+            /** forming request names like Request001, Request002 etc */
             request_ss << std::setw(3) << std::setfill('0') << request_count;
             request_count_string = request_ss.str();
             request_ss.str(std::string());
@@ -181,8 +224,6 @@ class ConverterJSON
             request_json.clear();
             request_count++;
         }
-
-
 
         final_json["answers"]=answers_json;
         std::ofstream json_file("answers.json");
@@ -196,19 +237,21 @@ int main()
 {
     ConverterJSON someStuff;
 
-    std::vector<std::vector<std::pair<int, float>>> answers;
+    auto recourses = someStuff.GetTextDocuments();
 
-    std::vector<std::pair<int, float>> a, b, c;
-    a.push_back(std::pair<int, float> (0, 0.892));
-    a.push_back(std::pair<int, float> (1, 0.452));
-    a.push_back(std::pair<int, float> (2, 0.231));
+    //std::vector<std::vector<std::pair<int, float>>> answers;
 
-    b.push_back(std::pair<int, float> (0, 0.777));
+    //std::vector<std::pair<int, float>> a, b, c;
+    //a.push_back(std::pair<int, float> (0, 0.892));
+    //a.push_back(std::pair<int, float> (1, 0.452));
+    //a.push_back(std::pair<int, float> (2, 0.231));
 
-    answers.push_back(a);
-    answers.push_back(c);
-    answers.push_back(b);
+    //b.push_back(std::pair<int, float> (0, 0.777));
 
-    someStuff.putAnswers(answers);
+    //answers.push_back(a);
+    //answers.push_back(c);
+    //answers.push_back(b);
+
+    //someStuff.putAnswers(answers);
     return 0;
 }
