f02652bcf4677443edefd066b96c1900b672b2f5
Evgenii.Korchagov <joedraiser@gmail.com>
2022-08-14 14:12:13 +0200

    InvertedIndex added + added test for it

---
+++ InvertedIndex.cpp
@@ -0,0 +1,56 @@
+#include "InvertedIndex.h"
+
+void InvertedIndex::UpdateDocumentBase(std::vector<std::string> input_docs)
+{
+    docs=input_docs;
+    for(auto freq_entry_for_each_string : freq_dictionary)
+    {
+        freq_entry_for_each_string.second = this->GetWordCount(freq_entry_for_each_string.first);
+    }
+}
+
+std::vector<Entry> InvertedIndex::GetWordCount(const std::string &word)
+{
+    std::vector<Entry> entryArray;
+    std::vector<std::thread> proccesingDocThreadArray;
+    std::mutex entryArrayLock;
+    int doc_id = 0;
+
+    auto proccessDocFunc = [&entryArray, &entryArrayLock, word](std::string doc, int doc_id)
+    {
+        int wordCount=0;
+        std::stringstream buff;
+        std::string word_to_check;
+
+        buff << doc;
+        while(!buff.eof())
+        {
+            buff >> word_to_check;
+            if(word_to_check == word)
+            {
+                wordCount++;
+            }
+        }
+        if(wordCount!=0)
+        {
+            entryArrayLock.lock();
+            entryArray.push_back(Entry() = {(size_t)doc_id, (size_t)wordCount});
+            entryArrayLock.unlock();
+        }
+    };
+
+
+
+    for(auto doc : docs)
+    {
+        proccesingDocThreadArray.push_back(std::thread(proccessDocFunc, doc, doc_id));
+        doc_id++;
+    }
+
+    for(int i=0;i<doc_id;i++)
+        proccesingDocThreadArray[i].join();
+
+    freq_dictionary[word]=entryArray;
+
+    return entryArray;
+}
--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 #include "ConverterJSON.h"
+#include "InvertedIndex.h"
 
 int main()
 {
     ConverterJSON someStuff;
 
-    auto recourses = someStuff.GetTextDocuments();
+    auto resources = someStuff.GetTextDocuments();
     auto requests = someStuff.GetRequests();
 
-    std::vector<std::vector<std::pair<int, float>>> answers;
+    //std::vector<std::vector<std::pair<int, float>>> answers;
 
-    std::vector<std::pair<int, float>> a, b, c;
-    a.push_back(std::pair<int, float> (0, 0.892));
-    a.push_back(std::pair<int, float> (1, 0.452));
-    a.push_back(std::pair<int, float> (2, 0.231));
+    //std::vector<std::pair<int, float>> a, b, c;
+    //a.push_back(std::pair<int, float> (0, 0.892));
+    //a.push_back(std::pair<int, float> (1, 0.452));
+    //a.push_back(std::pair<int, float> (2, 0.231));
 
-    b.push_back(std::pair<int, float> (0, 0.777));
+    //b.push_back(std::pair<int, float> (0, 0.777));
 
-    answers.push_back(a);
-    answers.push_back(c);
-    answers.push_back(b);
+    //answers.push_back(a);
+    //answers.push_back(c);
+    //answers.push_back(b);
 
-    someStuff.putAnswers(answers);
+    //someStuff.putAnswers(answers);
+
+    InvertedIndex invertedVar;
+    invertedVar.UpdateDocumentBase(resources);
+
+    invertedVar.GetWordCount("milk");
+
     return 0;
 }
